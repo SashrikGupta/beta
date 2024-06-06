de16bf82e61cf9c057b79e6513d025939fcc129c
Sashrik Gupta <159465707+SashrikGupta@users.noreply.github.com>
2024-06-06 21:33:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from pydantic_settings import BaseSettings
 from flask import Flask, request, jsonify
 from flask_cors import CORS  # Add this import for CORS support
 from moviepy.editor import AudioFileClip
